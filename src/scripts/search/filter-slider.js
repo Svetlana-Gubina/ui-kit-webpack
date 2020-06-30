7ee5b6e75dba38d48de6f4547b53f12dae15b698
Svetlana <gubinas83@gmail.com>
2020-06-30 18:21:13 +0300

    Datepicker customise WIP

--- src/scripts/search/filter-slider.js
+++ src/scripts/search/filter-slider.js
@@ -1,9 +1,21 @@
-import '../../../node_modules/air-datepicker/dist/css/datepicker.min.css';
 import '../../../node_modules/air-datepicker';
 
 // Datepicker
 
-$('#filterDates').datepicker([]);
+// showEvent: Тип события, по наступлению которого будет показан календарь, Значение по умолчанию "focus"
+
+$('#filterDates').datepicker({
+  minDate: new Date(),
+  range: true,
+  dateFormat: "dd M",
+  multipleDatesSeparator: " - ",
+  clearButton: true,
+  prevHtml: '<svg width="17" height="18" viewBox="0 0 17 18"><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"></path></svg>',
+  nextHtml: '<svg width="17" height="18" viewBox="0 0 17 18"><path d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z" fill="#BC9CFF"></path></svg>',
+  navTitles: {
+    days: 'MM  <i>yyyy</i>',
+  },
+});
 
 
 // Slider jQuery
@@ -178,7 +190,6 @@ const guestsDefaultOptions = getDefaultOptions(guestsIDs);
 
 $("#Guests").find(".calendar__btn--submit").on("click", function(evt) {
   evt.preventDefault();
-  const input = $("#Guests").parent().find($("#guestsInput"));
 });
 
  $(".Guests").on("click", function() {
